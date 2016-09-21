08836022657d3f79d07b54bcfcc2dff73c504419
Himanshu Mishra <himanshu.m786@gmail.com>
2016-09-22 00:16:25 +0530

    init seed app

---
+++ app.js
@@ -0,0 +1,11 @@
+(function(){
+	angular.module("app", ["ngMaterial"]);
+
+	angular.module("app")
+		.controller("MainController", ["$scope", MainController]);
+
+	function MainController($scope) {
+
+	}
+
+})();
